e9ea6911285bd60d9a713932f597254e0508105a
ninoweg <wegleitner.nino@gmx.at>
2024-01-02 22:01:39 +0100

    add gae

--- RL_Project/rl_modules/storage.py
+++ RL_Project/rl_modules/storage.py
@@ -19,9 +19,11 @@ class Storage:
                  obs_dim,
                  action_dim,
                  max_timesteps,
-                 gamma=0.98):
+                 gamma=0.98,
+                 lmbda=0.96):
         self.max_timesteps = max_timesteps
         self.gamma = gamma
+        self.lmbda = lmbda
 
         # create the buffer
         self.obs = np.zeros([self.max_timesteps, obs_dim])
@@ -51,16 +53,30 @@ class Storage:
     def clear(self):
         self.step = 0
 
-    def compute_returns(self, last_values):
+    def compute_returns(self, last_values, gae=False):
         for step in reversed(range(self.max_timesteps)):
-            if step == self.max_timesteps - 1:
-                next_values = last_values
+            diff = 0
+            if gae:
+                diff = self.max_timesteps - step
+                for l in range(diff):
+                    if step + l == self.max_timesteps - 1:
+                        next_values = last_values
+                    else:
+                        next_values = self.values[step + l + 1]
+                    next_is_not_terminate = 1.0 - self.dones[step + l]
+                    delta = self.rewards[step + l] + next_is_not_terminate * self.gamma * next_values - self.values[step + l] # calculate advantage estimation
+                    self.advantages[step] += ((self.gamma * self.lmbda)**l) * delta
             else:
-                next_values = self.values[step + 1]
-            next_is_not_terminate = 1.0 - self.dones[step]
-            delta = self.rewards[step] + next_is_not_terminate * self.gamma * next_values - self.values[step]
-            self.advantages[step] = delta
+                for step in reversed(range(self.max_timesteps)):
+                    if step == self.max_timesteps - 1:
+                        next_values = last_values
+                    else:
+                        next_values = self.values[step + 1]
+                    next_is_not_terminate = 1.0 - self.dones[step]
+                    delta = self.rewards[step] + next_is_not_terminate * self.gamma * next_values - self.values[step]
+                    self.advantages[step] = delta
             self.returns[step] = self.advantages[step] + self.values[step]
+
 
     def mini_batch_generator(self, num_batches, num_epochs=8, device="cpu"):
         batch_size = self.max_timesteps // num_batches
@@ -70,6 +86,7 @@ class Storage:
         actions = torch.from_numpy(self.actions).to(device).float()
         values = torch.from_numpy(self.values).to(device).float()
         advantages = torch.from_numpy(self.advantages).to(device).float()
+        actions_log_prob = torch.from_numpy(self.actions_log_prob).to(device).float()
 
         for epoch in range(num_epochs):
             for i in range(num_batches):
@@ -81,4 +98,5 @@ class Storage:
                 actions_batch = actions[batch_idx]
                 target_values_batch = values[batch_idx]
                 advantages_batch = advantages[batch_idx]
-                yield (obs_batch, actions_batch, target_values_batch, advantages_batch)
+                actions_log_prob_old_batch = actions_log_prob[batch_idx]
+                yield (obs_batch, actions_batch, target_values_batch, advantages_batch, actions_log_prob_old_batch)
